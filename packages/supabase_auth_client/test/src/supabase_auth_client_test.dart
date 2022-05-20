f4633db4d3072929f2052697829882477c0b5c90
Ana Polo <anna-91_@hotmail.com>
2022-05-20 17:20:48 +0200

    fix: change test dependency

--- packages/supabase_auth_client/test/src/supabase_auth_client_test.dart
+++ packages/supabase_auth_client/test/src/supabase_auth_client_test.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors
-import 'package:flutter_test/flutter_test.dart';
 import 'package:supabase_auth_client/supabase_auth_client.dart';
+import 'package:test/test.dart';
 
 void main() {
   group('SupabaseAuthClient', () {
