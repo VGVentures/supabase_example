cf1cd42f6bd1c68810988620ef3696330a766ff2
Ana Polo <anna-91_@hotmail.com>
2022-05-20 16:45:54 +0200

    fix: add keys to constructors

--- lib/counter/view/counter_page.dart
+++ lib/counter/view/counter_page.dart
@@ -23,7 +23,7 @@ class CounterPage extends StatelessWidget {
 }
 
 class CounterView extends StatelessWidget {
-  const CounterView();
+  const CounterView({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -51,7 +51,7 @@ class CounterView extends StatelessWidget {
 }
 
 class CounterText extends StatelessWidget {
-  const CounterText();
+  const CounterText({super.key});
 
   @override
   Widget build(BuildContext context) {
