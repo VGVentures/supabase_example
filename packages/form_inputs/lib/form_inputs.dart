5acfa97a4b0538b997562245a7b5b80c79cc2b87
Ana Polo <anna-91_@hotmail.com>
2022-05-20 19:41:55 +0200

    chore(form_input): add email input form

--- packages/form_inputs/lib/form_inputs.dart
+++ packages/form_inputs/lib/form_inputs.dart
@@ -1,3 +1,3 @@
 library form_inputs;
 
-export 'src/form_inputs.dart';
+export 'src/email.dart';
--- packages/form_inputs/test/src/form_inputs_test.dart
+++ packages/form_inputs/test/src/form_inputs_test.dart
@@ -3,9 +3,43 @@ import 'package:form_inputs/form_inputs.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('FormInputs', () {
-    test('can be instantiated', () {
-      expect(FormInputs(), isNotNull);
+  const emailString = 'test@test.com';
+  group('Email', () {
+    group('constructors', () {
+      test('pure creates correct instance', () {
+        final email = Email.pure();
+        expect(email.value, '');
+        expect(email.isPure, true);
+      });
+
+      test('dirty creates correct instance', () {
+        final email = Email.dirty(emailString);
+        expect(email.value, emailString);
+        expect(email.isPure, false);
+      });
+    });
+
+    group('validator', () {
+      test('returns invalid error when email is empty', () {
+        expect(
+          Email.dirty().error,
+          EmailValidationError.invalid,
+        );
+      });
+
+      test('returns invalid error when email is malformed', () {
+        expect(
+          Email.dirty('test').error,
+          EmailValidationError.invalid,
+        );
+      });
+
+      test('is valid when email is valid', () {
+        expect(
+          Email.dirty(emailString).error,
+          isNull,
+        );
+      });
     });
   });
 }
