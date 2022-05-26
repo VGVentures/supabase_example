0d59402b20b352e370b42b0167f9b955fce7d90b
Ana Polo <anna-91_@hotmail.com>
2022-05-26 17:21:13 +0200

    fix: resolve pr comments

--- packages/supabase_database_client/test/src/models/supabase_user_test.dart
+++ packages/supabase_database_client/test/src/models/supabase_user_test.dart
@@ -6,12 +6,45 @@ import 'package:supabase_database_client/src/models/models.dart';
 void main() {
   group('SupabaseUser', () {
     test('supports value equality', () {
-      final supabaseUserA = SupabaseUser(userName: 'A', companyName: 'B');
-      final secondSupabaseUserA = SupabaseUser(userName: 'A', companyName: 'B');
-      final supabaseUserB = SupabaseUser(userName: 'B', companyName: 'C');
+      final supabaseUserA =
+          SupabaseUser(id: 'id', userName: 'A', companyName: 'B');
+      final secondSupabaseUserA =
+          SupabaseUser(id: 'id', userName: 'A', companyName: 'B');
+      final supabaseUserB =
+          SupabaseUser(id: '', userName: 'B', companyName: 'C');
 
       expect(supabaseUserA, equals(secondSupabaseUserA));
       expect(supabaseUserA, isNot(equals(supabaseUserB)));
+    });
+
+    test('can be (de)serialized', () {
+      final supabaseUser = SupabaseUser(
+        id: 'id',
+        userName: 'user name',
+        companyName: 'company name',
+      );
+      final json = {
+        'id': 'id',
+        'username': 'user name',
+        'companyname': 'company name'
+      };
+
+      expect(SupabaseUser.fromJson(json), equals(supabaseUser));
+    });
+
+    test('can be (de)serialized', () {
+      final supabaseUser = SupabaseUser(
+        id: 'id',
+        userName: 'user name',
+        companyName: 'company name',
+      );
+      final json = {
+        'id': 'id',
+        'username': 'user name',
+        'companyname': 'company name'
+      };
+
+      expect(supabaseUser.toJson(), equals(json));
     });
   });
 }
