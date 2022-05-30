e33edb1084f59391b6d4c644df475c558cd8588c
Ana Polo <anna-91_@hotmail.com>
2022-05-30 13:41:02 +0200

    feat(email_launcher): email launcher package

---
+++ packages/email_launcher/lib/src/email_launcher.dart
@@ -0,0 +1,7 @@
+/// {@template email_launcher}
+/// A package to open an external email app on Android and iOS.
+/// {@endtemplate}
+class EmailLauncher {
+  /// {@macro email_launcher}
+  const EmailLauncher();
+}
